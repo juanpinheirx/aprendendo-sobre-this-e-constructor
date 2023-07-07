5058a9cafc81596b841e39c3cee5a9450678ddb4
juanpinheirx <jroliveirapinheiro@gmail.com>
2023-07-07 12:49:19 -0300

    aprendendo sobre state e setState

--- src/App.js
+++ src/App.js
@@ -17,14 +17,38 @@ class App extends React.Component {
     console.log(this, 'this aplicado antes da render');
     this.handleThis = this.handleThis.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      cliques: 0,
+    }
+    this.handleCliques = this.handleCliques.bind(this);
+    this.handleCliques1 = this.handleCliques1.bind(this);
+    this.handleCliques2 = this.handleCliques2.bind(this);
+    //este é o método o qual se define o estado de cada componente.
+  }
+
+  handleCliques() {
+    this.setState({
+      cliques: 1,
+    })
+    //a função setState faz com que o o estado se atualize. Porém atualiza SEMPRE para 1.
+  }
+
+  handleCliques1() {
+    this.setState((estadoInicial, _props) => ({
+      cliques: estadoInicial.cliques +1
+    }))
+  } //esta é a forma com que o estado será INCREMENTADO +1 a cada click no botão cinco.
+
+  handleCliques2() {
+    this.setState((state, prop) => ({
+      cliques: state.cliques +1
+    })) // esta função faz com que o botão, na tela, seja incrementado +1.
   }
 
   handleThis() {
     console.log(this);
     console.log('this aplicado por conta da construção');
-  }
-
-  //a função handleThis consegue ler as informações do this por que está dentro da função construtora.
+  } //a função handleThis consegue ler as informações do this por que está dentro da função construtora.
 
   handleChange() {
     console.log('Clicou!');
@@ -42,6 +66,11 @@ class App extends React.Component {
       <button onClick={handleClick}>Meu botao</button>
       <button onClick={this.handleChange}>Meu botao dois</button>
       <button onClick={this.handleThis}>meu botao três</button>
+      <button onClick={this.handleCliques}>meu botão quatro</button>
+      <button onClick={this.handleCliques1} >meu botão cinco</button>
+      <button onClick={this.handleCliques2}
+      //este evento onClick, chamando o this.state.cliques (estado inicial), faz com que na tela seja incrementado o numero de cliques.
+      >{this.state.cliques}</button>
     </>
   );
 }
