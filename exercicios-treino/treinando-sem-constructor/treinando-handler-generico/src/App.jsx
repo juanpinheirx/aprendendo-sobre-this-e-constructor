d15fd6251950be1e369fe6545f15a2f8b13d812d
juanpinheirx <jroliveirapinheiro@gmail.com>
2023-07-14 10:16:36 -0300

    handlerGenerico refatorado para tornar o estado imutável

--- exercicios-treino/treinando-sem-constructor/treinando-handler-generico/src/App.jsx
+++ exercicios-treino/treinando-sem-constructor/treinando-handler-generico/src/App.jsx
@@ -12,11 +12,23 @@ class App extends React.Component {
       willGo: false,
   };
 
+  // handleChange = ({target}) => {
+  //   const {name} = target;
+  //   const value = target.type === 'checkbox' ? target.checked : target.value;
+  //   this.setState({
+  //     [name]: value, // esta é a função que faz a mudança de estado tanto para os inputs de tipo texto quanto para os inputs de tipo checkbox.
+  //   })
+  // }
+
   handleChange = ({target}) => {
-    const {name} = target;
+    // neste handler refatorado, vou refazer a função handle utilizando o Object.assign({}) que pega um objeto vazio e atribui os valores do estado para este objeto como uma cópia.
+    const { name } = target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
-    this.setState({
-      [name]: value,
+    this.setState((prev) => {
+      const copyState = Object.assign({}, prev, {
+        [name]: value,
+      });
+      return copyState; // com este handler, eu faço uma cópia do estado, tornando este imutável como na documentação do react recomenda. para evitar futuros problemas.
     })
   }
   
