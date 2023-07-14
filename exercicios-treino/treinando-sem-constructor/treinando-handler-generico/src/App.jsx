d8e6f431f270edca18e7bfc9d77a69cb3fcdb5c0
juanpinheirx <jroliveirapinheiro@gmail.com>
2023-07-14 10:43:32 -0300

    handlerGenerico refatorado para tornar o estado imutável

--- exercicios-treino/treinando-sem-constructor/treinando-handler-generico/src/App.jsx
+++ exercicios-treino/treinando-sem-constructor/treinando-handler-generico/src/App.jsx
@@ -4,12 +4,12 @@ import "./App.css";
 
 class App extends React.Component {
   state = {
-      name: '',
-      email: "",
-      age: 0,
-      func: "",
-      motive: "",
-      willGo: false,
+    name: "",
+    email: "",
+    age: 0,
+    func: "",
+    motive: "",
+    willGo: false,
   };
 
   // handleChange = ({target}) => {
@@ -20,22 +20,23 @@ class App extends React.Component {
   //   })
   // }
 
-  handleChange = ({target}) => {
+  handleChange = ({ target }) => {
     // neste handler refatorado, vou refazer a função handle utilizando o Object.assign({}) que pega um objeto vazio e atribui os valores do estado para este objeto como uma cópia.
     const { name } = target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const value = target.type === "checkbox" ? target.checked : target.value;
     this.setState((prev) => {
       const copyState = Object.assign({}, prev, {
         [name]: value,
       });
       return copyState; // com este handler, eu faço uma cópia do estado, tornando este imutável como na documentação do react recomenda. para evitar futuros problemas.
-    })
-  }
-  
+    });
+  };
+
   render() {
     return (
-      <Form value={this.state}
-      handleChange={this.handleChange} //essa foi a forma a qual o estado e a função handleChange foi enviada via PROPS.
+      <Form
+        value={this.state}
+        handleChange={this.handleChange} //essa foi a forma a qual o estado e a função handleChange foi enviada via PROPS.
       />
     );
   }
